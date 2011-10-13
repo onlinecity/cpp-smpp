e82629bf721d08b216ba015a8c974cb38868f9cf
Thomas Duerlund <td@onlinecity.dk>
2011-10-13 09:17:29 +0200

    Relative timeparsing.

--- src/timeformat.h
+++ src/timeformat.h
@@ -13,6 +13,55 @@ using namespace boost::gregorian;
 namespace smpp {
 namespace timeformat {
 
+ptime parseRelativeTimestamp(const smatch &match)
+{
+	cout << "relative" << endl;
+	for (unsigned int i = 0 ; i < match.size() ; i++) {
+		cout << i << ":" << match[i] << endl;
+	}
+
+	stringstream ss;
+	ss << match[1];
+
+	long yy;
+	ss >> yy;
+	ss.clear();
+
+	long mon;
+	ss.str("");
+	ss << match[2];
+	ss >> mon;
+	ss.clear();
+
+	long dd;
+	ss.str("");
+	ss << match[3];
+	ss >> dd;
+	ss.clear();
+
+	long hh;
+	ss.str("");
+	ss << match[4];
+	ss >> hh;
+	ss.clear();
+
+	long min;
+	ss.str("");
+	ss << match[5];
+	ss >> min;
+	ss.clear();
+
+	long sec;
+	ss.str("");
+	ss << match[6];
+	ss >> sec;
+
+	ptime now(second_clock::local_time());
+	ptime relative = now + years(yy) + months(mon) + days(dd) + hours(hh) + minutes(min) + seconds(sec);
+
+	return relative;
+}
+
 ptime convert(const string &time)
 {
 	// Matches the pattern“YYMMDDhhmmsstnnp”
@@ -20,47 +69,13 @@ ptime convert(const string &time)
 	smatch match;
 
 	if (regex_match(time.begin(), time.end(), match, pattern)) {
-		cout << "match" << endl;
-		cout << (match) << endl;
-		for (int i = 1 ; i < match.size() ; i++) {
-			cout << match[i] << endl;
+
+		for (unsigned int i = 0 ; i < match.size() ; i++) {
+			cout << i << ":" << match[i] << endl;
 		}
 
 		if (match[match.size() - 1] == "R") {
-			cout << "relative" << endl;
-			stringstream ss(match[1]);
-			long yy;
-			ss >> yy;
-			ss.clear();
-
-			long mon;
-			ss.str(match[2]);
-			ss >> mon;
-			ss.clear();
-
-			long dd;
-			ss.str(match[3]);
-			ss >> dd;
-			ss.clear();
-
-			long hh;
-			ss.str(match[4]);
-			ss >> hh;
-			ss.clear();
-
-			long min;
-			ss.str(match[5]);
-			ss >> min;
-
-			long sec;
-			ss.str(match[6]);
-			ss >> sec;
-
-			ptime now(second_clock::local_time());
-			ptime relative = now + years(yy) + months(mon) + days(dd) + hours(hh) + minutes(min) + seconds(sec);
-			cout << "now      :" << now << endl;
-			cout << "relative :" << relative << endl;
-
+			return parseRelativeTimestamp(match);
 		}
 
 	}
@@ -68,6 +83,7 @@ ptime convert(const string &time)
 	posix_time::ptime test;
 	return test;
 }
+
 }
 }
 
