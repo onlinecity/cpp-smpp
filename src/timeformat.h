332048443c1697fce25ae36e0e8b4da7144ab8a4
Thomas Duerlund <td@onlinecity.dk>
2011-10-14 09:37:22 +0200

    Added some comments

--- src/timeformat.h
+++ src/timeformat.h
@@ -1,6 +1,7 @@
 #ifndef TIMEFORMAT_H_
 #define TIMEFORMAT_H_
 
+#include "exceptions.h"
 #include <boost/regex.hpp>
 #include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
@@ -17,8 +18,7 @@ typedef pair<local_time::local_date_time, posix_time::time_duration> DatePair;
 
 /**
  * Parses a relative timestamp and returns it as a time_duration.
- *
- * @param match
+ * @param match Relative timestamp on the form of a boost::regex::smatch
  * @return time_duration representation of the timestamp.
  */
 time_duration parseRelativeTimestamp(const smatch &match)
@@ -65,8 +65,8 @@ time_duration parseRelativeTimestamp(const smatch &match)
 }
 
 /**
- * Parses a absolute timestamp and returns it as a local_date_time.
- * @param match
+ * Parses an absolute timestamp and returns it as a local_date_time.
+ * @param match Absolute timestmp on the form of a boost::regex::smatch
  * @return local_date_time representation of the timestamp.
  */
 local_time::local_date_time parseAbsoluteTimestamp(const smatch &match)
@@ -92,12 +92,15 @@ local_time::local_date_time parseAbsoluteTimestamp(const smatch &match)
 
 /**
  * Parses a smpp timestamp and returns a DatePair representation of the timestamp.
+ * The smpp timestamp has the following format: “YYMMDDhhmmsstnnp”, an SmppException
+ * thrown if the input timeformat differs.
  * @param time
  * @return
+ * @throw SmppException.
  */
 DatePair parseSmppTimestamp(const string &time)
 {
-	// Matches the pattern“YYMMDDhhmmsstnnp”
+	// Matches the pattern “YYMMDDhhmmsstnnp”
 	regex pattern("^(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{1})(\\d{2})([R+-])$", regex_constants::perl);
 	smatch match;
 	if (regex_match(time.begin(), time.end(), match, pattern)) {
@@ -123,7 +126,9 @@ DatePair parseSmppTimestamp(const string &time)
 			return DatePair(ldt, td);
 		}
 	} else {
-		throw std::exception();
+		stringstream ss;
+		ss << "Timestamp \"" << time << "\" has the wrong format.";
+		throw smpp::SmppException(ss.str());
 	}
 }
 
