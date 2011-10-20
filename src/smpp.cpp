eafa6d275e0c86375e02be52a2a01b6798f479f8
Thomas Duerlund <td@onlinecity.dk>
2011-10-20 09:58:27 +0200

    Namespace fixes

--- src/smpp.cpp
+++ src/smpp.cpp
@@ -1,106 +1,105 @@
 #include "smpp.h"
-
 using namespace std;
-namespace smapp {
 
+namespace smpp {
 string getEsmeStatus(uint32_t i)
 {
 	switch (i) {
-		case smpp::ESME_ROK:
+		case ESME_ROK:
 			return "No Error";
-		case smpp::ESME_RINVMSGLEN:
+		case ESME_RINVMSGLEN:
 			return "Message Length is invalid";
-		case smpp::ESME_RINVCMDLEN:
+		case ESME_RINVCMDLEN:
 			return "Command Length is invalid";
-		case smpp::ESME_RINVCMDID:
+		case ESME_RINVCMDID:
 			return "Invalid Command ID";
-		case smpp::ESME_RINVBNDSTS:
+		case ESME_RINVBNDSTS:
 			return "Incorrect BIND Status for given command";
-		case smpp::ESME_RALYBND:
+		case ESME_RALYBND:
 			return "ESME Already in Bound State";
-		case smpp::ESME_RINVPRTFLG:
+		case ESME_RINVPRTFLG:
 			return "Invalid Priority Flag";
-		case smpp::ESME_RINVREGDLVFLG:
+		case ESME_RINVREGDLVFLG:
 			return "Invalid Registered Delivery Flag";
-		case smpp::ESME_RSYSERR:
+		case ESME_RSYSERR:
 			return "System Error";
-		case smpp::ESME_RINVSRCADR:
+		case ESME_RINVSRCADR:
 			return "Invalid Source Address";
-		case smpp::ESME_RINVDSTADR:
+		case ESME_RINVDSTADR:
 			return "Invalid Dest Addr";
-		case smpp::ESME_RINVMSGID:
+		case ESME_RINVMSGID:
 			return "Message ID is invalid";
-		case smpp::ESME_RBINDFAIL:
+		case ESME_RBINDFAIL:
 			return "Bind Failed";
-		case smpp::ESME_RINVPASWD:
+		case ESME_RINVPASWD:
 			return "Invalid Password";
-		case smpp::ESME_RINVSYSID:
+		case ESME_RINVSYSID:
 			return "Invalid System ID";
-		case smpp::ESME_RCANCELFAIL:
+		case ESME_RCANCELFAIL:
 			return "Cancel SM Failed";
-		case smpp::ESME_RREPLACEFAIL:
+		case ESME_RREPLACEFAIL:
 			return "Replace SM Failed";
-		case smpp::ESME_RMSGQFUL:
+		case ESME_RMSGQFUL:
 			return "Message Queue Full";
-		case smpp::ESME_RINVSERTYP:
+		case ESME_RINVSERTYP:
 			return "Invalid Service Type";
-		case smpp::ESME_RINVNUMDESTS:
+		case ESME_RINVNUMDESTS:
 			return "Invalid number of destinations";
-		case smpp::ESME_RINVDLNAME:
+		case ESME_RINVDLNAME:
 			return "Invalid Distribution List name";
-		case smpp::ESME_RINVDESTFLAG:
+		case ESME_RINVDESTFLAG:
 			return "Destination flag (submit_multi)";
-		case smpp::ESME_RINVSUBREP:
+		case ESME_RINVSUBREP:
 			return "Invalid ‘submit with replace’ request (i.e. submit_sm with replace_if_present_flag set)";
-		case smpp::ESME_RINVESMSUBMIT:
+		case ESME_RINVESMSUBMIT:
 			return "Invalid esm_SUBMIT field data";
-		case smpp::ESME_RCNTSUBDL:
+		case ESME_RCNTSUBDL:
 			return "Cannot Submit to Distribution List";
-		case smpp::ESME_RSUBMITFAIL:
+		case ESME_RSUBMITFAIL:
 			return "submit_sm or submit_multi failed";
-		case smpp::ESME_RINVSRCTON:
+		case ESME_RINVSRCTON:
 			return "Invalid Source address TON";
-		case smpp::ESME_RINVSRCNPI:
+		case ESME_RINVSRCNPI:
 			return "Invalid Source address NPI";
-		case smpp::ESME_RINVDSTTON:
+		case ESME_RINVDSTTON:
 			return "Invalid Destination address TON";
-		case smpp::ESME_RINVDSTNPI:
+		case ESME_RINVDSTNPI:
 			return "Invalid Destination address NPI";
-		case smpp::ESME_RINVSYSTYP:
+		case ESME_RINVSYSTYP:
 			return "Invalid system_type field";
-		case smpp::ESME_RINVREPFLAG:
+		case ESME_RINVREPFLAG:
 			return "Invalid replace_if_present flag";
-		case smpp::ESME_RINVNUMMSGS:
+		case ESME_RINVNUMMSGS:
 			return "Invalid number of messages";
-		case smpp::ESME_RTHROTTLED:
+		case ESME_RTHROTTLED:
 			return "Throttling error (ESME has exceeded allowed message limits)";
-		case smpp::ESME_RINVSCHED:
+		case ESME_RINVSCHED:
 			return "Invalid Scheduled Delivery Time";
-		case smpp::ESME_RINVEXPIRY:
+		case ESME_RINVEXPIRY:
 			return "Invalid message (Expiry time)";
-		case smpp::ESME_RINVDFTMSGID:
+		case ESME_RINVDFTMSGID:
 			return "Predefined Message Invalid or Not Found";
-		case smpp::ESME_RX_T_APPN:
+		case ESME_RX_T_APPN:
 			return "ESME Receiver Temporary App Error Code";
-		case smpp::ESME_RX_P_APPN:
+		case ESME_RX_P_APPN:
 			return "ESME Receiver Permanent App Error Code";
-		case smpp::ESME_RX_R_APPN:
+		case ESME_RX_R_APPN:
 			return "ESME Receiver Reject Message Error Code";
-		case smpp::ESME_RQUERYFAIL:
+		case ESME_RQUERYFAIL:
 			return "query_sm request failed";
-		case smpp::ESME_RINVOPTPARSTREAM:
+		case ESME_RINVOPTPARSTREAM:
 			return "Error in the optional part of the PDU Body.";
-		case smpp::ESME_ROPTPARNOTALLWD:
+		case ESME_ROPTPARNOTALLWD:
 			return "Optional Parameter not allowed";
-		case smpp::ESME_RINVPARLEN:
+		case ESME_RINVPARLEN:
 			return "Invalid Parameter Length.";
-		case smpp::ESME_RMISSINGOPTPARAM:
+		case ESME_RMISSINGOPTPARAM:
 			return "Expected Optional Parameter missing";
-		case smpp::ESME_RINVOPTPARAMVAL:
+		case ESME_RINVOPTPARAMVAL:
 			return "Invalid Optional Parameter Value";
-		case smpp::ESME_RDELIVERYFAILURE:
+		case ESME_RDELIVERYFAILURE:
 			return "Delivery Failure (data_sm_resp)";
-		case smpp::ESME_RUNKNOWNERR:
+		case ESME_RUNKNOWNERR:
 			return "Unknown Error";
 
 		default:
