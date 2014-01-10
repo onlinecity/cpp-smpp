4b3a624f3ce5dc56e77cbac72f6947e1e75ef249
Thomas Duerlund <td@onlinecity.dk>
2014-01-10 12:30:27 +0100

    GTest and lint

--- test/livetest.cpp
+++ test/livetest.cpp
@@ -6,11 +6,10 @@
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ClientTest);
 
-int main(int argc, char **argv)
-{
-	CppUnit::TextUi::TestRunner runner;
-	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
-	runner.addTest(registry.makeTest());
-	return runner.run() ? 0 : 1;
+int main(int argc, char **argv) {
+    CppUnit::TextUi::TestRunner runner;
+    CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+    runner.addTest(registry.makeTest());
+    return runner.run() ? 0 : 1;
 }
 
