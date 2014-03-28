4f0ef460705574487fc4013b25a3ba7506473825
Thomas Duerlund <td@onlinecity.dk>
2014-03-28 13:55:22 +0100

    YCM

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,11 @@
+import os.path
+execfile(os.path.expanduser("~/.vim/bundle/YouCompleteMe/cpp/ycm/.ycm_extra_conf.py"))
+
+flags.extend([
+'-I',
+'src',
+'-I',
+'ext/src/googletest/include',
+'-I',
+'third_party/src/asio/include'
+])
